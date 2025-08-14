1deb511099b752b0d7582044676b87bf02a7bf0b
JeongwooSeo <shipfriend4371@naver.com>
2025-08-14 20:47:03 +0900

    refactor: mock데이터 분리

---
+++ src/constants/Store.ts
@@ -0,0 +1,26 @@
+import MockStore1 from "@/assets/images/store/mockStore1.png";
+import MockStore2 from "@/assets/images/store/mockStore2.png";
+import MockStore3 from "@/assets/images/store/mockStore3.webp";
+
+const mockStore1 = {
+  id: 1,
+  storeTitle: "포롱",
+  ownerId: 1,
+  thumbnailURL: MockStore1,
+};
+
+const mockStore2 = {
+  id: 2,
+  storeTitle: "오비",
+  ownerId: 1,
+  thumbnailURL: MockStore2,
+};
+
+const mockStore3 = {
+  id: 3,
+  storeTitle: "스타벅스",
+  ownerId: 1,
+  thumbnailURL: MockStore3,
+};
+
+export const mockStores = [mockStore1, mockStore2, mockStore3];
