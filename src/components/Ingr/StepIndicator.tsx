60a0e3acc78c060b2417c68a01a6b637e10a42a6
JeongwooSeo <shipfriend4371@naver.com>
2025-08-26 01:56:20 +0900

    refactor: 기존 StepIndicator를 여러 스텝에 대응할 수 있도록 기능 추가

--- src/components/Ingr/StepIndicator.tsx
+++ src/components/Ingr/StepIndicator.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { ingrStyles } from '@/src/styles/IngrStyle';
+import React from "react";
+import { View, Text } from "react-native";
+import { ingrStyles } from "@/src/styles/IngrStyle";
 
-const StepIndicator = () => (
-    <View style={ingrStyles.stepWrapper}>
-        <View style={ingrStyles.stepCircle}>
-            <Text style={ingrStyles.stepText}>1</Text>
+interface StepIndicatorProps {
+  currentStep?: number;
+  maxSteps?: number;
+}
+
+const StepIndicator = ({
+  currentStep = 1,
+  maxSteps = 2,
+}: StepIndicatorProps) => (
+  <View style={ingrStyles.stepWrapper}>
+    {Array(maxSteps)
+      .fill(0)
+      .map((_, index) => (
+        <View
+          key={index}
+          style={[
+            ingrStyles.stepCircle,
+            {
+              backgroundColor: index <= currentStep ? "#268AFF" : "#E0E0E0",
+            },
+          ]}
+        >
+          <Text
+            style={{
+              color: index <= currentStep ? "#FFFFFF" : "#A0A0A0",
+              fontWeight: "bold",
+            }}
+          >
+            {index + 1}
+          </Text>
         </View>
-        <View style={[ingrStyles.stepCircle, { backgroundColor: '#E0E0E0' }]}>
-            <Text style={{ color: '#A0A0A0', fontWeight: 'bold' }}>2</Text>
-        </View>
-    </View>
+      ))}
+  </View>
 );
 
 export default StepIndicator;
