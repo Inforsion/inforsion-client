8ff2e8ad1eabe2a725b7ee8f60d03641165aeb76
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2025-08-25 20:21:19 +0900

    feat: 웹뷰 컴포넌트 로딩 UI 추가

--- src/components/webview/ChartWebView.tsx
+++ src/components/webview/ChartWebView.tsx
@@ -1,5 +1,5 @@
 import WebView from "react-native-webview";
-import { StyleSheet, View } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Colors } from "@/src/constants/Colors";
 
@@ -19,6 +19,16 @@ const ChartWebView = ({ url }: ChartWebViewProps) => {
         javaScriptEnabled
         domStorageEnabled
         scrollEnabled={false}
+        startInLoadingState={true}
+        renderLoading={() => (
+          <View style={styles.loadingContainer}>
+            <ActivityIndicator
+              color="#009688"
+              size="large"
+              style={{ flex: 1 }}
+            />
+          </View>
+        )}
       />
     </View>
   );
@@ -27,8 +37,26 @@ const styles = StyleSheet.create({
   chartContainer: {
     backgroundColor: Colors.light.background.secondary,
     height: 200,
-    borderRadius: 12,
+    width: "100%",
     overflow: "hidden",
+  },
+  loadingContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: Colors.light.background.secondary,
+    width: "100%",
+    height: 200,
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 1,
+  },
+  loadingText: {
+    fontSize: 14,
+    color: Colors.light.text.secondary,
+    fontWeight: "500",
   },
 });
 
