23d60d329ae055f4d065e178e5256fb5179c7fe4
Gaeun Yun <100302974+yungan9@users.noreply.github.com>
2025-08-29 18:05:36 +0900

    chore: 선언했던 Tabs.Screen 제거

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -47,8 +47,6 @@ export default function RootLayout() {
         />
         <Stack.Screen name="+not-found" />
         <Stack.Screen name="splash_intro" options={{headerShown: false}} />
-        <Stack.Screen name="ingr" options={{headerShown: false}} />
-          <Stack.Screen name="recipe" options={{headerShown: false}} />
       </Stack>
       <StatusBar backgroundColor={"#fff"} />
     </ThemeProvider>
